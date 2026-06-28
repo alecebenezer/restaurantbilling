2c50031163e20a329b39e04d2859edba0a0449d7
Alec Ebenezer <alecebenezer7@gmail.com>
2026-06-28 15:29:58 +0530

    updated navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <Link to="/">Dashboard</Link>
+   
 
       <Link to="/menu">Menu</Link>
 
-      <Link to="/orders">Orders</Link>
+      
 
-      <Link to="/billing">Billing</Link>
+      
 
-      <Link to="/invoices">Invoices</Link>
     </nav>
   );
 }
