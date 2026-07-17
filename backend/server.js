5a594a8fb77b40293c9783c388007dc657cb5d35
Alec Ebenezer <alecebenezer7@gmail.com>
2026-07-17 20:50:37 +0530

    fix route filename

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import orderRoutes from "./routes/orderRoutes.js";
+import orderRoutes from "./routes/orderroutes.js";
 
 dotenv.config();
 
